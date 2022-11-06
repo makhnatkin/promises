daaa6fcc613071782293e9f8008582e03ee076af
Sergey Makhnatkin <sergey.makhnatkin@gmail.com>
2022-11-07 02:08:10 +0300

    Added step-3

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,16 @@
 "use strict";
 
 // sayHello();
-const loadScript = src => {
+const loadScript = (src, callback) => {
     let script = document.createElement('script');
     script.src = src;
+    script.onload = () => callback(script);
     document.head.append(script);
 }
 
 // загрузит и выполнит скрипт
-loadScript('./scripts/script.js');
+loadScript('./scripts/script.js', () => {
 
-// не выполнится - ошибка
-// ReferenceError: sayHello is not defined
-sayHello();
+    // работает
+    sayHello();
+});
