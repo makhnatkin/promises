fa67b210a1885621adba7d6f18dfea3c4e0a2df0
Sergey Makhnatkin <sergey.makhnatkin@gmail.com>
2022-11-07 02:23:33 +0300

    Added step-5

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,21 @@
 "use strict";
 
 // sayHello();
-const loadScript = (src, callback) => {
+function loadScript(src, callback) {
     let script = document.createElement('script');
     script.src = src;
-    script.onload = () => callback(script);
+
+    script.onload = () => callback(null, script);
+    script.onerror = () => callback(new Error(`Ошибка загрузки скрипта ${src}`));
+
     document.head.append(script);
 }
-
 // загрузит и выполнит скрипт
-loadScript('./scripts/script.js', () => {
-    loadScript('./scripts/script-2.js', () => {
-        loadScript('./scripts/script-3.js', () => {
-            loadScript('./scripts/script-4.js', () => {
-                loadScript('./scripts/script-5.js', () => {
-
-                    // работает
-                    sayHello();
-                });
-            });
-        });
-    });
+loadScript('./scripts/script.js', (error) => {
+    if (error) {
+        console.error(error);
+    } else {
+        // работает
+        sayHello();
+    }
 });
