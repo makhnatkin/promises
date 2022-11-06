7f222bcd6448d9db271368d75f1077e8a626de21
Sergey Makhnatkin <sergey.makhnatkin@gmail.com>
2022-11-07 02:36:17 +0300

    Added step-7

--- scripts/index.js
+++ scripts/index.js
@@ -12,9 +12,22 @@ const loadScript = src => new Promise(function(resolve, reject) {
 
 let promise = loadScript("https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.11/lodash.js");
 
-promise.then(
-    script => alert(`${script.src} загружен!`),
-    error => alert(`Ошибка: ${error.message}`)
-);
-
-promise.then(script => alert('Ещё один обработчик...'));
+promise
+    .then(
+        script => alert(`${script.src} загружен!`),
+        error => alert(`Ошибка: ${error.message}`)
+    )
+    .then(script => {
+        alert('Ещё один обработчик...');
+        return 'Результат 1'
+    })
+    .then(str => {
+        alert(str);
+        return 'Результат 2'
+    })
+    .then(str => {
+        alert(str);
+        return 'Результат 3'
+    })
+    .catch(error => alert('Ошибка' + error))
+    .finally((str = '') => alert('Все завершено' + str))
